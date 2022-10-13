e37cbf7842f65c967ed5d5dc1ff72360d308a861
hamnerben <hamnerben@gmail.com>
2022-10-12 19:43:07 -0600

    initialize repo

---
+++ src/PoetryMain.java
@@ -0,0 +1,12 @@
+
+public class PoetryMain {
+    public static void main(String[] args) {
+
+        WritePoetry poem = new WritePoetry();
+        System.out.println(poem.WritePoem("green.txt", "sam", 20, true));
+        System.out.println(poem.WritePoem("Lester.txt", "lester", 30, true));
+        System.out.println(poem.WritePoem("HowMany.txt", "how", 30, false));
+        System.out.println(poem.WritePoem("Zebra.txt", "are", 50, true));
+    }
+
+}
---
+++ src/WordFreqInfo.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class WordFreqInfo {
+    public String word;
+    public int occurCt;
+    ArrayList<Freq> followList;
+
+    public WordFreqInfo(String word, int count) {
+        this.word = word;
+        this.occurCt = count;
+        this.followList = new ArrayList<Freq>();
+    }
+
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Word :" + word+":");
+        sb.append(" (" + occurCt + ") : ");
+        for (Freq f : followList)
+            sb.append(f.toString());
+
+        return sb.toString();
+    }
+
+    public void updateFollows(String follow) {
+       //System.out.println("updateFollows " + word + " " + follow);
+        occurCt++;
+        for (Freq f : followList) {
+            if (follow.compareTo(f.follow) == 0) {
+                f.followCt++;
+                return;
+            }
+        }
+        followList.add(new Freq(follow, 1));
+    }
+
+    public static class Freq {
+        String follow;
+        int followCt;
+
+        public Freq(String follow, int ct) {
+            this.follow = follow;
+            this.followCt = ct;
+        }
+
+        public String toString() {
+            return follow + " [" + followCt + "] ";
+        }
+
+        public boolean equals(Freq f2) {
+            return this.follow.equals(f2.follow);
+        }
+    }
+
+
+}
+
