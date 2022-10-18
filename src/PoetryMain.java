12061a9aeb49a4152b881d475823d3bb806abec3
hamnerben <hamnerben@gmail.com>
2022-10-17 20:53:30 -0600

    okay I think I read everything

--- src/PoetryMain.java
+++ src/PoetryMain.java
@@ -2,11 +2,11 @@
 public class PoetryMain {
     public static void main(String[] args) {
 
-        WritePoetry poem = new WritePoetry();
-        System.out.println(poem.WritePoem("green.txt", "sam", 20, true));
-        System.out.println(poem.WritePoem("Lester.txt", "lester", 30, true));
-        System.out.println(poem.WritePoem("HowMany.txt", "how", 30, false));
-        System.out.println(poem.WritePoem("Zebra.txt", "are", 50, true));
+//        WritePoetry poem = new WritePoetry();
+//        System.out.println(poem.WritePoem("green.txt", "sam", 20, true));
+//        System.out.println(poem.WritePoem("Lester.txt", "lester", 30, true));
+//        System.out.println(poem.WritePoem("HowMany.txt", "how", 30, false));
+//        System.out.println(poem.WritePoem("Zebra.txt", "are", 50, true));
     }
 
 }
